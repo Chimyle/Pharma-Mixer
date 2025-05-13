243fbf92f3d623d649640e1c10196ea5375de999
Chimyle <edventosa@up.edu.ph>
2025-05-13 16:04:16 +0800

    Added comments to explain stuff

--- NonI2C_Mixer/NonI2C_Mixer.ino
+++ NonI2C_Mixer/NonI2C_Mixer.ino
@@ -1,7 +1,9 @@
+// Libraries
 #include <Keypad.h>
 #include <LiquidCrystal_I2C.h>
 #include <RTClib.h>
 
+// Pin Assignment
 #define LED_BUILTIN 1
 #define PUL_PIN 33
 #define DIR_PIN 32
@@ -14,8 +16,8 @@
 #define PWM_RESOLUTION 8  // 8-bit resolution
 #define PWM_DUTY_CYCLE 127 // 50% duty cycle
 
-LiquidCrystal_I2C lcd(0x27, 20, 4);
-RTC_DS3231 rtc;
+LiquidCrystal_I2C lcd(0x27, 20, 4);     //Setup LCD Display
+RTC_DS3231 rtc;                         //Setup RTC
 
 // Keypad setup
 const byte ROWS = 4;
@@ -30,12 +32,9 @@ char keys[ROWS][COLS] = {
 byte rowPins[ROWS] = {2, 0, 4, 16};  // R1-R4
 byte colPins[COLS] = {17, 5, 18, 19};    // C1-C4
 
-// byte rowPins[ROWS] = {19, 18, 5, 17};  // R1-R4
-// byte colPins[COLS] = {16, 4, 0, 2};    // C1-C4
-
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
-// Time and control variables
+// Setup RTC
 String timeBuffer = "";
 int mins = 0;
 int secs = 0;
@@ -46,15 +45,15 @@ DateTime endTime;
 // Gear ratio constants
 const float gear_ratio = 60.0 / 20.0;
 
-//IR Sensor
+//IR Sensor Variables
 const float SampRate = 3.0; //RPM Calc interval in seconds
 const float PPR = 12.0; //Pulse per revolution of IR disc encoder
 volatile unsigned long pulseCount = 0;
 volatile unsigned long calctimer = 0;
 portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;
-
 volatile unsigned long lastPulseTime = 0;
-void IRAM_ATTR handlePulse() {
+
+void IRAM_ATTR handlePulse() {        //Interrup Service Routine (ISR) for Counting RPM pulses
   unsigned long now = micros();
   if (now - lastPulseTime > 5000) {  // debounce: ignore pulses within 5ms
     portENTER_CRITICAL_ISR(&mux);
@@ -65,11 +64,12 @@ void IRAM_ATTR handlePulse() {
 }
 
 
-void IRAM_ATTR sqwISR() {
+void IRAM_ATTR sqwISR() {            //ISR for RTC counter
   calctimer++;
 }
 
 void setup() {
+  //Pin Setup
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);  // Turn LED off (active low)
   pinMode(PUL_PIN, OUTPUT);
@@ -77,14 +77,17 @@ void setup() {
   pinMode(ENA_PIN, OUTPUT);
   pinMode(SENSOR_PIN, INPUT);
   attachInterrupt(digitalPinToInterrupt(SENSOR_PIN), handlePulse, FALLING);
-
+  
+  //Motor Driver Initial Conditions
   digitalWrite(ENA_PIN, LOW);  // Enable motor driver
   digitalWrite(DIR_PIN, HIGH); // Set default direction
 
+  //LCD Initialization
   Wire.begin();
   lcd.init();
   lcd.backlight();
 
+  //RTC Initialization
   if (!rtc.begin()) {
     lcd.setCursor(0, 1);
     lcd.print("RTC not found!");
@@ -93,42 +96,43 @@ void setup() {
   rtc.writeSqwPinMode(DS3231_SquareWave1Hz);
   pinMode(SQW_PIN, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(SQW_PIN), sqwISR, FALLING);
-
-  SetupPWM();
-  Reset();
+  
+  SetupPWM();    //Runs the function that sets up Motor Configuration
+  Reset();       //Reset Function
 }
 
 void loop() {
-  char key = keypad.getKey();
-  if (key) {
-    if (key >= '0' && key <= '9') {
-      InputTimer(key);
+  char key = keypad.getKey();             //Stores character pressed on the keypad
+  if (key) {                              //Executes when any key is pressed
+    if (key >= '0' && key <= '9') {       
+      InputTimer(key);                    //Accepts input for timer duration. Only works if a number is pressed
     } 
-    else if (key == '#') {
-      if (timeBuffer.length() == 4) {
+    else if (key == '#') {                //OK Key
+      if (timeBuffer.length() == 4) {     //Checks if time is input completely/correctly
         mins = timeBuffer.substring(0, 2).toInt();
         secs = timeBuffer.substring(2, 4).toInt();
-        SelectRPM();
+        SelectRPM();                      //Moves to selecting the RPM after storing duration
       } else {
         lcd.setCursor(0, 1);
-        lcd.print("Invalid Time   ");
+        lcd.print("Invalid Time   ");     //Error if incomplete timer input
         delay(2000);
-        Reset();
+        Reset();                          //Resets after 2 seconds
       }
     } 
-    else if (key == '*') {
+    else if (key == '*') {                //Clear Key
       Reset();
     }
   }
 }
 
 void InputTimer(char key) {
-  if (timeBuffer.length() >= 4) {
+  if (timeBuffer.length() >= 4) {        //If the input is more than 4 digits, reset
     timeBuffer = "";
   }
 
-  timeBuffer += key;
+  timeBuffer += key;                     //digits are concatenated after every press
 
+  //Codes for displaying the input
   String displayTime = timeBuffer;
   while (displayTime.length() < 4) displayTime += "_";
   String formatted = displayTime.substring(0, 2) + ":" + displayTime.substring(2, 4);
@@ -141,6 +145,7 @@ void SelectRPM() {
   lcd.setCursor(0, 2);
   lcd.print(" RPM :           ");  // Clear RPM line
 
+  //RPM selection based on the key pressed
   while (true) {
     char key = keypad.getKey();
     if (key) {
@@ -157,20 +162,22 @@ void SelectRPM() {
         case 'D':
           Setpoint = 90;
           break;
-        case '#':
+        case '#':                    //OK Key
           if (Setpoint > 0) {
             if (mins > 0 || secs > 0) {
-              endTime = rtc.now() + TimeSpan(0, 0, mins, secs);
+              //Setting end time and starting motor. Only works if RPM and duration is not 0
+              endTime = rtc.now() + TimeSpan(0, 0, mins, secs);            
               RunMotor();
               return;
             }
           }
           break;
-        case '*':
+        case '*':        //Reset key. Clears all variables
           Reset();
           return;
       }
 
+      //Displaying the RPM Value
       if (Setpoint > 0) {
         lcd.setCursor(8, 2);
         lcd.print("      ");
@@ -182,6 +189,7 @@ void SelectRPM() {
 }
 
 void RunMotor() {
+  //Update Display to show time left, RPM, and controls
   lcd.setCursor(0, 1);
   lcd.print("Time Left: ");
   lcd.setCursor(0, 2);
@@ -196,36 +204,43 @@ void RunMotor() {
 
   SetMotorPWM(Setpoint); // Start PWM based on selected RPM
 
-  while (rtc.now() < endTime) {
+  while (rtc.now() < endTime) {          //Timer. This system runs only this when timer is active.
     char key = keypad.getKey();
     if (key) {
-      switch (key) {
-        case '#':
+      switch (key) {                     // Checking for Stop and Pause keypress
+        case '#':                        //Stop button. Stops the motor and clears the variables
           StopMotorPWM();
           digitalWrite(ENA_PIN, HIGH); // Disable motor
           Reset();
           return;
           break;
-        case '*':
+        case '*':                        // Pause button
           StopMotorPWM();
           digitalWrite(ENA_PIN, HIGH); // Disable motor
           DateTime now = rtc.now();
-          TimeSpan remaining = endTime - now;
+          TimeSpan remaining = endTime - now;          //Calculate remaining time
+
+          //Display for Pause Screen
           lcd.setCursor(0, 2);
           lcd.print("    -- PAUSED --    ");
+          lcd.setCursor(0, 3);
+          lcd.print("  *-RESUME  #-STOP  ");
+          
           while (true) {
-            endTime = rtc.now() + TimeSpan(0, remaining.hours(), remaining.minutes(), remaining.seconds());
+            endTime = rtc.now() + TimeSpan(0, remaining.hours(), remaining.minutes(), remaining.seconds());      //continuously update the end time while paused
             char unpause = keypad.getKey();
             if (unpause) {
-              if (unpause == '#'){
+              if (unpause == '#'){            //Stop button. Stops the motor and clears the variables
                 StopMotorPWM();
                 digitalWrite(ENA_PIN, HIGH); // Disable motor
                 Reset();
                 return;
                 break;
-              }else{
+              }else if(unpause == '*'){       //Unpause key. Continues the current process until timer
                 lcd.setCursor(0, 2);
                 lcd.print("RPM:                ");  
+                //lcd.setCursor(0, 3);
+                //lcd.print("  *-PAUSE   #-STOP  ");
                 RunMotor();
                 return;
               }
@@ -234,13 +249,15 @@ void RunMotor() {
           break;
       }
     }
-
+    //Updates the display every 0.2 seconds
     if (millis() - lastUpdateLCD >= 200) {
       lastUpdateLCD = millis();
       UpdateCountdownLCD();
     }
   }
-
+  
+  /* TIMER ENDS HERE */
+  
   StopMotorPWM();
   digitalWrite(ENA_PIN, HIGH); // Disable motor
 
@@ -249,7 +266,7 @@ void RunMotor() {
   lcd.setCursor(0, 2);
   lcd.print("Done! Press to Reset");
 
-  while (true) {
+  while (true) {              //Waits for keypress before going back to input screen
     if (keypad.getKey()) {
       Reset();
       return;
@@ -269,6 +286,7 @@ void UpdateCountdownLCD() {
   lcd.print("         ");
   lcd.setCursor(12, 1);
   lcd.print(buffer);
+  
   //Update RPM
   if (calctimer >= SampRate) {
     lcd.setCursor(13, 2);
